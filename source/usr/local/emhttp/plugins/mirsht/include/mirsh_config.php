1e20d1858e2b4394788814a6fbd5e0fb6b629b6d
desertwitch <24509509+desertwitch@users.noreply.github.com>
2025-06-11 10:34:39 +0200

    changes

--- source/usr/local/emhttp/plugins/mirsht/include/mirsh_config.php
+++ source/usr/local/emhttp/plugins/mirsht/include/mirsh_config.php
@@ -18,6 +18,17 @@
  *
  */
 
-$mirsh_backend = htmlspecialchars(trim(shell_exec("find /var/log/packages/ -type f -iname 'mirrorshuttle-*' -printf '%f\n' 2> /dev/null") ?? "n/a"));
+$mirsh_cfg = file_exists("/boot/config/plugins/mirsh/mirsh.cfg") ? parse_ini_file("/boot/config/plugins/mirsh/mirsh.cfg") : [];
 
+$mirsh_cron = trim(isset($mirsh_cfg['CRON']) ? htmlspecialchars($mirsh_cfg['CRON']) : 'disable');
+$mirsh_cronhour = trim(isset($mirsh_cfg['CRONHOUR']) ? htmlspecialchars($mirsh_cfg['CRONHOUR']) : '1');
+$mirsh_crondow = trim(isset($mirsh_cfg['CRONDOW']) ? htmlspecialchars($mirsh_cfg['CRONDOW']) : '0');
+$mirsh_crondom = trim(isset($mirsh_cfg['CRONDOM']) ? htmlspecialchars($mirsh_cfg['CRONDOM']) : '1');
+
+$mirsh_startnotify = trim(isset($mirsh_cfg['STARTNOTIFY']) ? htmlspecialchars($mirsh_cfg['STARTNOTIFY']) : 'disable');
+$mirsh_finishnotify = trim(isset($mirsh_cfg['FINISHNOTIFY']) ? htmlspecialchars($mirsh_cfg['FINISHNOTIFY']) : 'enable');
+$mirsh_errornotify = trim(isset($mirsh_cfg['ERRORNOTIFY']) ? htmlspecialchars($mirsh_cfg['ERRORNOTIFY']) : 'enable');
+
+$mirsh_backend = htmlspecialchars(trim(shell_exec("find /var/log/packages/ -type f -iname 'mirrorshuttle-*' -printf '%f\n' 2> /dev/null") ?? "n/a"));
+$mirsh_running = !empty(shell_exec("pgrep -x mirrorshuttle 2>/dev/null"));
 ?>
